3b6f6d928591af6746d6a33f51c21a31b7f96167
Nate Hellman <natehellman@MacBook-Pro.local>
2016-11-07 23:26:30 -0800

    app.js tweak

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
 var db = mongoose.connection;
 
-var mongodbUri = 'mongodb://heroku_gvffrvsx:h3nci6gv759n3p1br6unjkqu4k@ds145997.mlab.com:45997/heroku_gvffrvsx';
-
-mongoose.connect(mongodbUri);
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://heroku_gvffrvsx:h3nci6gv759n3p1br6unjkqu4k@ds145997.mlab.com:45997/heroku_gvffrvsx');
 
 var User = require('./models/user.js');
 
