57a6705b4eafa80cf4fedc042a5e43b2878e9f26
Nate Hellman <natehellman@MacBook-Pro.local>
2016-11-07 19:35:58 -0800

    updated app.js

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
 var db = mongoose.connection;
 
-mongoose.connect('mongodb://localhost/knowledge');
-
 var User = require('./models/user.js');
 
 var routes = require('./routes/index');
@@ -88,6 +86,22 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+app.use(session({
+    secret: 'secret_key',
+    cookie: {maxAge: 1209600000},
+
+    // store: new MongoStore({
+    //   'db': 'meetupplanner'
+    // }),
+
+    store: new MongoStore({
+      url: process.env.MONGOLAB_URI
+    }),
+
+    resave: true,
+    saveUninitialized: true
+  }));
 
 
 module.exports = app;
